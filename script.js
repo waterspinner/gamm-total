bcec509ed5fd17574eabbb17e8a0e8af1e6113ce
waterspinner <98507468+waterspinner@users.noreply.github.com>
2023-03-16 14:29:56 -0700

    Added logic to fetch total gamm

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-let pools;
+const pools = {};
+let poolBtn = document.querySelector('.pool-submit');
+
+window.addEventListener('load', () => {
+    fetch('https://api.osl.zone/osmosis/gamm/v1beta1/pools?pagination.limit=9999')
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        for(e in data.pools){
+            pools[data.pools[e].id] = {amount: data.pools[e].total_shares.amount};
+        }
+        poolBtn.addEventListener('click', () => {
+            let poolId = document.querySelector('.pool-number')
+            console.log(poolId.value)
+            console.log(pools[poolId.value].amount)
+            displayGamm = document.querySelector('.gamm-total');
+            displayGamm.innerHTML = 'Total gamm:' + pools[poolId.value].amount;
+    })
+      })
+})
+
 
